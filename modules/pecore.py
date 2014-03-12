b96966b7e93e58768d345100e22e60ac5582a1b2
Gianni Amato <guelfoweb@gmail.com>
2014-03-13 00:34:11 +0100

    Update pecore.py

--- modules/pecore.py
+++ modules/pecore.py
@@ -3,18 +3,18 @@
 # ----------------------------------------------------------------------
 # This file is part of PEFrame.
 #
-# Knock is free software: you can redistribute it and/or modify
+# PEFrame is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Knock is distributed in the hope that it will be useful,
+# Peframe is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Knock. If not, see <http://www.gnu.org/licenses/>.
+# along with peframe. If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------
 
 import re
