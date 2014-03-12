7ed2cd6cfe860d52ebf51fa20e7ceb9decba8738
Gianni Amato <guelfoweb@gmail.com>
2014-03-13 00:33:40 +0100

    Update peframe.py

--- peframe.py
+++ peframe.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 
-##############################################################
+# PEFrame
 #
-## PEFrame: Portable Executable Framework
-##
-## Gianni 'guelfoweb' Amato - guelfoweb@gmail.com
-##
-## https://github.com/guelfoweb/peframe
-##
-## Licence: GNU GPL v.2.0
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+# PEFrame is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-##############################################################
+# Peframe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with peframe. If not, see <http://www.gnu.org/licenses/>.
 
 import os, sys
 import time, datetime
