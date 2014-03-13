587e8c9e309c7b1a21b8b081f0c1d64252cd0e17
Gianni Amato <guelfoweb@gmail.com>
2014-03-13 02:45:15 +0100

    Update peframe.py

--- peframe.py
+++ peframe.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-# PEFrame
+# PEframe
 #
-# PEFrame is free software: you can redistribute it and/or modify
+# PEframe is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Peframe is distributed in the hope that it will be useful,
+# PEframe is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with peframe. If not, see <http://www.gnu.org/licenses/>.
+# along with PEframe. If not, see <http://www.gnu.org/licenses/>.
 
 import os, sys
 import time, datetime
@@ -27,7 +27,7 @@ except ImportError:
 	sys.exit(0)
 
 # INFORMATION
-NAME		= "PEFrame"
+NAME		= "PEframe"
 VERSION		= "3.0 rc1"
 AUTHOR		= "Author: Gianni 'guelfoweb' Amato"
 GITHUB		= "Github: https://github.com/guelfoweb/peframe"
