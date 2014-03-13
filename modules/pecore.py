86a4f5376cd2e6db340313eb04608c1c82ef1713
Gianni Amato <guelfoweb@gmail.com>
2014-03-13 02:45:51 +0100

    Update pecore.py

--- modules/pecore.py
+++ modules/pecore.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
 # ----------------------------------------------------------------------
-# This file is part of PEFrame.
+# This file is part of PEframe.
 #
-# PEFrame is free software: you can redistribute it and/or modify
+# PEframe is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Peframe is distributed in the hope that it will be useful,
+# PEframe is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with peframe. If not, see <http://www.gnu.org/licenses/>.
+# along with PEframe. If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------
 
 import re
