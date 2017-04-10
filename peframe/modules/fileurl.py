774974ee3df7d7716ad5af4dd0a99131c1e885c8
Damian Ignacio <damian.ignacio@gmail.com>
2017-04-10 20:53:04 -0300

    Mixing spaces and hard tabs.

--- peframe/modules/fileurl.py
+++ peframe/modules/fileurl.py
@@ -38,86 +38,85 @@ def valid_ip(address):
         return False
 
 def get(filename, strings_match):
-	strings_info = json.loads(stringstat.get(filename))
-	strings_list = strings_info['content']
-	ip_list = []
-	file_list = []
-	filetype_dict = {}
-	url_list = []
-	fuzzing_dict = {}
-	apialert_list = []
-	antidbg_list = []
+    strings_info = json.loads(stringstat.get(filename))
+    strings_list = strings_info['content']
+    ip_list = []
+    file_list = []
+    filetype_dict = {}
+    url_list = []
+    fuzzing_dict = {}
+    apialert_list = []
+    antidbg_list = []
 
-	# Get filetype and fuzzing
-	file_type = strings_match['filetype'].items()
-	fuzzing_list = strings_match['fuzzing'].items()
+    # Get filetype and fuzzing
+    file_type = strings_match['filetype'].items()
+    fuzzing_list = strings_match['fuzzing'].items()
 
-	# Strings analysis
-	for string in strings_list:
-		# URL list
-		urllist = re.findall(r'((smb|srm|ssh|ftps?|file|https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', string, re.MULTILINE)
-		if urllist:
-			for url in urllist:
-				url_list.append(url[0])
+    # Strings analysis
+    for string in strings_list:
+        # URL list
+        urllist = re.findall(r'((smb|srm|ssh|ftps?|file|https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', string, re.MULTILINE)
+        if urllist:
+            for url in urllist:
+                url_list.append(url[0])
 
-		# IP list
-		iplist = re.findall(r'[0-9]+(?:\.[0-9]+){3}', string, re.MULTILINE)
-		if iplist:
-			for ip in iplist:
-				if valid_ip(str(ip)) and not re.findall(r'[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}\.0', str(ip)):
-					ip_list.append(str(ip))
+        # IP list
+        iplist = re.findall(r'[0-9]+(?:\.[0-9]+){3}', string, re.MULTILINE)
+        if iplist:
+            for ip in iplist:
+                if valid_ip(str(ip)) and not re.findall(r'[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}\.0', str(ip)):
+                    ip_list.append(str(ip))
 
-		# FILE list
-		fname = re.findall("(.+(\.([a-z]{2,3}$)|\/.+\/|\\\.+\\\))+", string, re.IGNORECASE | re.MULTILINE)
-		if fname:
-			for word in fname:
-				word = filter(None, word[0])
-				file_list.append(word)
+        # FILE list
+        fname = re.findall("(.+(\.([a-z]{2,3}$)|\/.+\/|\\\.+\\\))+", string, re.IGNORECASE | re.MULTILINE)
+        if fname:
+            for word in fname:
+                word = filter(None, word[0])
+                file_list.append(word)
 
-	# Purge list
-	ip_list = filter(None, list(set([item for item in ip_list])))
-	url_list = filter(None, list(set([item for item in url_list])))
-	
-	# Initialize filetype
-	for key, value in file_type:
-		filetype_dict[key] = []
+    # Purge list
+    ip_list = filter(None, list(set([item for item in ip_list])))
+    url_list = filter(None, list(set([item for item in url_list])))
 
-	# Search for valid filename
-	array_tmp = []
-	for file in file_list:
-		for key, value in file_type:
-			match = re.findall("\\"+value+"$", file, re.IGNORECASE | re.MULTILINE)
-			if match and file.lower() not in array_tmp and len(file) > 4: 
-				filetype_dict[key].append(file)
-				array_tmp.append(file.lower())
-	
-	# Remove empty key filetype
-	for key, value in filetype_dict.items():
-		if not filetype_dict[key]:
-			del filetype_dict[key]
+    # Initialize filetype
+    for key, value in file_type:
+        filetype_dict[key] = []
 
-	# Initialize fuzzing
-	for key, value in fuzzing_list:
-		fuzzing_dict[key] = []
+    # Search for valid filename
+    array_tmp = []
+    for file in file_list:
+        for key, value in file_type:
+            match = re.findall("\\"+value+"$", file, re.IGNORECASE | re.MULTILINE)
+            if match and file.lower() not in array_tmp and len(file) > 4:
+                filetype_dict[key].append(file)
+                array_tmp.append(file.lower())
 
-	# Strings analysis for fuzzing
-	array_tmp = []
-	for string in strings_list:
-		for key, value in fuzzing_list:
-			fuzz_match = re.findall(value, string, re.IGNORECASE | re.MULTILINE)
-			if fuzz_match and string.lower() not in array_tmp:
-				fuzzing_dict[key].append(string)
-				array_tmp.append(string.lower())
+    # Remove empty key filetype
+    for key, value in filetype_dict.items():
+        if not filetype_dict[key]:
+            del filetype_dict[key]
 
-	# Remove empty key filetype
-	for key, value in filetype_dict.items():
-		if not filetype_dict[key]:
-			del filetype_dict[key]
+    # Initialize fuzzing
+    for key, value in fuzzing_list:
+        fuzzing_dict[key] = []
 
-	# Remove empty key fuzzing
-	for key, value in fuzzing_list:
-		if not fuzzing_dict[key]:
-			del fuzzing_dict[key]
-	
-	return {"file":  filetype_dict, "url": url_list, "ip": ip_list, "fuzzing": fuzzing_dict}
-		
+    # Strings analysis for fuzzing
+    array_tmp = []
+    for string in strings_list:
+        for key, value in fuzzing_list:
+            fuzz_match = re.findall(value, string, re.IGNORECASE | re.MULTILINE)
+            if fuzz_match and string.lower() not in array_tmp:
+                fuzzing_dict[key].append(string)
+                array_tmp.append(string.lower())
+
+    # Remove empty key filetype
+    for key, value in filetype_dict.items():
+        if not filetype_dict[key]:
+            del filetype_dict[key]
+
+    # Remove empty key fuzzing
+    for key, value in fuzzing_list:
+        if not fuzzing_dict[key]:
+            del fuzzing_dict[key]
+
+    return {"file":  filetype_dict, "url": url_list, "ip": ip_list, "fuzzing": fuzzing_dict}
