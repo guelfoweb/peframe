1b4b92da1f06bccad97f04718451948c1bb54299
Gianni Amato <guelfoweb@gmail.com>
2019-07-09 14:40:37 +0200

    Install package for Termux Android

---
+++ install-peframe-termux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------
+# This file is part of peframe https://github.com/guelfoweb/peframe/
+# ----------------------------------------------------------------------
+
+peframe_version="6.0.3"
+environment_test="Termux Android"
+
+echo "Installation script peframe $peframe_version"
+echo -e "Tested on $environment_test\n"
+
+read -rsp $'Press enter to continue...\n'
+
+pkg update -y
+pkg upgrade -y
+
+pkg install -y git
+pkg install -y python
+pkg install -y python-dev
+pkg install -y clang
+pkg install -y swig
+pkg install -y openssl-dev
+pkg install -y libffi-dev
+pkg install -y sox
+
+git clone https://github.com/guelfoweb/peframe.git
+cd peframe
+python3 setup.py install
